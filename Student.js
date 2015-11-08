1f0458e29580a548c12ace4613f928b645013518
Karl Jansen <jnsnkrl@live.com>
2015-11-07 17:13:44 -0800

    initial commit

---
+++ Student.js
@@ -0,0 +1,12 @@
+/**
+ * method = public
+ * _method = private
+ */
+ 
+ function student() {
+  this.id = "";
+  this.nameFirst = "";
+  this.nameLast = "";
+  this.display = "";
+  this.element = null;
+}
---
+++ Studentbox.js
@@ -0,0 +1,11 @@
+/**
+ * method = public
+ * _method = private
+ */
+ 
+ function studentbox() {
+  this.id = "";
+  this.label = "";
+  this.count = 0;
+  this.element = null;
+}
